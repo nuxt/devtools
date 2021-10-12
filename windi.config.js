e5691b049b684ae98a04c5a324679d0b1f34053b
Sébastien Chopin <seb@nuxtjs.com>
2021-10-12 12:29:10 +0200

    fix: updates colors and templates

--- windi.config.js
+++ windi.config.js
@@ -1,30 +1,68 @@
+const colors = require('windicss/colors')
+
 module.exports = {
   purge: ['./src/**/*.html'],
   darkMode: 'media',
   theme: {
     colors: {
-      white: '#ffffff',
-      // Shades
-      cloud: {
-        surface: '#E6F0F0',
-        lightest: '#D1E2E2',
-        lighter: '#B2CCCC',
-        light: '#92ADAD',
-        DEFAULT: '#688282',
-        dark: '#566B6B',
-        darker: '#334040',
-        darkest: '#1A2121'
+      white: 'white',
+      black: 'black',
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: {
+        50: '#F2FDF9',
+        100: '#E6FCF3',
+        200: '#BFF6E0',
+        300: '#99F1CD',
+        400: '#4DE7A8',
+        DEFAULT: '#00DC82',
+        600: '#00C675',
+        700: '#00844E',
+        800: '#00633B',
+        900: '#004227'
       },
-      sand: {
-        surface: '#F5F5EB',
-        lightest: '#EDEDD5',
-        lighter: '#DEDEB6',
-        light: '#B4B48C',
-        DEFAULT: '#9C9C6A',
-        dark: '#636330',
-        darker: '#3B3B00',
-        darkest: '#1F2100'
+      'secondary-surface': '#E5F9FF',
+      'secondary-lightest': '#B7E1ED',
+      'secondary-lighter': '#95CDDE',
+      'secondary-light': '#71A2B0',
+      secondary: '#497A87',
+      'secondary-dark': '#255461',
+      'secondary-darker': '#003543',
+      'secondary-darkest': '#012A35',
+      'secondary-black': '#001E26',
+      tertiary: '#B2CCCC', // cloud
+      'cloud-surface': '#E6F0F0',
+      'cloud-lightest': '#D1E2E2',
+      'cloud-lighter': '#B2CCCC',
+      'cloud-light': '#92ADAD',
+      cloud: '#688282',
+      'cloud-dark': '#566B6B',
+      'cloud-darker': '#334040',
+      'cloud-darkest': '#273131',
+      'cloud-black': '#1A2121',
+      black: '#000',
+      white: '#fff',
+      blue: colors.sky,
+      green: {
+        // 50: "#eefdf2",
+        50: '#d0fcde',
+        100: '#b0fccb',
+        200: '#8cfab7',
+        300: '#64f4a3',
+        400: '#37e990',
+        500: '#00d77d',
+        600: '#00bb6a',
+        700: '#009956',
+        800: '#047342',
+        900: '#134d2e'
+        // 950: "#132a1c",
       },
+      red: colors.red,
+      rose: colors.rose,
+      yellow: colors.amber,
+      orange: colors.orange,
+      gray: colors.gray,
+      purple: colors.purple,
       sky: {
         surface: '#E5F9FF',
         lightest: '#B7E1ED',
@@ -32,92 +70,9 @@ module.exports = {
         light: '#71A2B0',
         DEFAULT: '#497A87',
         dark: '#255461',
-        darker: '#002E3B',
-        darkest: '#001E26'
-      },
-      mint: {
-        surface: '#E7FEFD',
-        lightest: '#C5EEEC',
-        lighter: '#A8DDDB',
-        light: '#7EB1B0',
-        DEFAULT: '#558887',
-        dark: '#2E6160',
-        darker: '#003C3C',
-        darkest: '#002021'
-      },
-      stone: {
-        surface: '#F6F6F6',
-        lightest: '#D4D4D4',
-        lighter: '#B2B2B2',
-        light: '#909090',
-        DEFAULT: '#6E6E6E',
-        dark: '#4C4C4C',
-        darker: '#2A2A2A',
-        darkest: '#080808'
-      },
-      // Colors
-      green: {
-        lighter: '#80EEC0',
-        light: '#40E5A1',
-        DEFAULT: '#00DC82',
-        dark: '#00BB6F',
-        darker: '#009A5B'
-      },
-      lime: {
-        lighter: '#DEE9A7',
-        light: '#CEDF7B',
-        DEFAULT: '#BDD44F',
-        dark: '#A1B443',
-        darker: '#849437'
-      },
-      yellow: {
-        lighter: '#FFF2AC',
-        light: '#FFEC83',
-        DEFAULT: '#FFE65A',
-        dark: '#D9C34D',
-        darker: '#B3A13F'
-      },
-      orange: {
-        lighter: '#F9C580',
-        light: '#F7A840',
-        DEFAULT: '#F48B00',
-        dark: '#CF7600',
-        darker: '#AB6100'
-      },
-      red: {
-        lighter: '#FFB1A3',
-        light: '#FF8B74',
-        DEFAULT: '#FF6446',
-        dark: '#D9553C',
-        darker: '#B34631'
-      },
-      pink: {
-        lighter: '#E0B5DF',
-        light: '#D190CE',
-        DEFAULT: '#C26BBE',
-        dark: '#A55BA2',
-        darker: '#884B85'
-      },
-      indigo: {
-        lighter: '#80B1E3',
-        light: '#408BD6',
-        DEFAULT: '#0466C8',
-        dark: '#0055AA',
-        darker: '#00468C'
-      },
-      blue: {
-        lighter: '#94D5F7',
-        light: '#5EBFF4',
-        DEFAULT: '#28AAF0',
-        dark: '#2291CC',
-        darker: '#1C77A8'
-      },
-      teal: {
-        lighter: '#80F2F2',
-        light: '#40EBEB',
-        DEFAULT: '#00E5E5',
-        dark: '#00C3C3',
-        darker: '#00A0A0'
+        darker: '#003543',
+        darkest: '#012A35',
+        black: '#001E26'
       }
     }
   }
