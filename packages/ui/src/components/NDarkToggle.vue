ecf8a694ea0fdead6c9d3b8276a0e2eb6b682d26
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-23 20:02:15 +0800

    feat(ui): new NDarkToggle renderless component helper

---
+++ packages/ui/src/components/NDarkToggle.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+/* eslint-disable vue/no-multiple-template-root */
+import { useColorMode, useToggle } from '@vueuse/core'
+import { computed } from 'vue'
+
+const mode = useColorMode()
+
+const isDark = computed<boolean>({
+  get () {
+    return mode.value === 'dark'
+  },
+  set (v) {
+    mode.value = v ? 'dark' : 'light'
+  }
+})
+
+const toggle = useToggle(isDark)
+
+const context = {
+  mode,
+  isDark,
+  toggle
+}
+</script>
+
+<template>
+  <slot v-bind="context" />
+</template>
