204d0ceca5b19c98ae5328e2b0905bada15612ae
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-24 14:02:05 +0800

    chore: fix linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['@nuxtjs/eslint-config-typescript'],
   rules: {
     'vue/require-default-prop': 0,
+    'vue/multi-word-component-names': 0,
     'import/no-named-as-default-member': 0
   }
 }
