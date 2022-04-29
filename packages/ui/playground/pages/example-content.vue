51a2875b09683209dd8e4ccea1df39a1637bb593
Clément Ollivier <clement.o2p@gmail.com>
2022-04-29 11:43:07 +0200

    feat(ui): extend example layout (#98)
    * feat(ui): extend example layout
    
    * feat(ui): remove content v1 icon and use text

---
+++ packages/ui/playground/pages/example-content.vue
@@ -0,0 +1,8 @@
+<template>
+  <NuxtExampleLayout example="test" repo="nuxt/content-next">
+    <template #icon>
+      @Nuxt/Content
+    </template>
+    <div>Hi</div>
+  </NuxtExampleLayout>
+</template>
