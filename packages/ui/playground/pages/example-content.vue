e3d332e12f684794a7b0e1307f654247b5781fa2
Sébastien Chopin <seb@nuxtjs.com>
2022-06-24 12:27:51 +0200

    feat: add Typography preset (#130)
    Resolves #129

--- packages/ui/playground/pages/example-content.vue
+++ packages/ui/playground/pages/example-content.vue
@@ -1,5 +1,8 @@
 <template>
   <NuxtExampleLayout example="test" repo="nuxt/content">
-    <div>Hi</div>
+    <div class="prose text-left">
+      <h1>Hi</h1>
+      <p>This is some content</p>
+    </div>
   </NuxtExampleLayout>
 </template>
