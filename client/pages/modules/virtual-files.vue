fe4fad132fb3125c44f63648eebc23b02ecad36f
Anthony Fu <anthonyfu117@hotmail.com>
2023-01-24 16:00:23 +0100

    feat: search for virtual files

--- client/pages/modules/virtual-files.vue
+++ client/pages/modules/virtual-files.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import Fuse from 'fuse.js'
+
 definePageMeta({
   icon: 'carbon-assembly-reference',
   title: 'Virtual Files',
@@ -16,6 +18,8 @@ interface VfsFile {
   id: string
   content: string
 }
+
+const searchString = $ref('')
 
 const { data, refresh } = await useFetch<VfsData>('/_vfs.json', {
   key: 'vfs-list',
@@ -44,20 +48,37 @@ function toShortPath(path: string) {
     : path
 }
 
-const files = computed(() => {
+const files = $computed(() => {
   if (!data.value)
     return []
   return data.value.entries
     // Hide Nuxt dist files, as they are aliased as `#build`
     .filter(i => !i.id.startsWith(`${data.value?.rootDir || ''}/.nuxt/`))
+    .sort((a, b) => a.id.localeCompare(b.id))
+})
+
+const fuse = $computed(() => new Fuse(files, {
+  keys: [
+    'id',
+    'path',
+  ],
+}))
+
+const filteredFiles = $computed(() => {
+  if (!searchString)
+    return files
+  return fuse.search(searchString).map(i => i.item)
 })
 </script>
 
 <template>
   <div grid="~ cols-[250px_1fr]" h-full of-hidden class="virtual-files">
     <div border="r base" flex="~ col" of-auto>
+      <div p3 pb2>
+        <NTextInput v-model="searchString" placeholder="Search..." n="primary" />
+      </div>
       <NuxtLink
-        v-for="f of files" :key="f.id" px2 py1 border="b base" text-sm font-mono
+        v-for="f of filteredFiles" :key="f.id" px2 py1 border="b base" text-sm font-mono
         :to="`/modules/virtual-files?id=${encodeURIComponent(f.id)}`"
         :class="f.id === current?.id ? 'bg-truegray:20 text-base' : 'text-truegray'"
       >
@@ -65,7 +86,7 @@ const files = computed(() => {
       </NuxtLink>
     </div>
     <pre v-if="current?.content" of-auto h-full text-sm v-html="highlight(current?.content, 'typescript')" />
-    <pre v-else flex items-center justify-center>Select one file to start</pre>
+    <span v-else flex items-center justify-center op50>Select one file to start</span>
   </div>
 </template>
 
