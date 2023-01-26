09ebc3b82f00fa4416279f7b80f6cfb0a62acf74
Anthony Fu <anthonyfu117@hotmail.com>
2023-01-26 17:20:44 +0100

    chore: clean up

--- src/integrations/vite-inspect.ts
+++ src/integrations/vite-inspect.ts
@@ -1,6 +1,6 @@
 import { addVitePlugin } from '@nuxt/kit'
 import type { Nuxt } from '@nuxt/schema'
-import type { ViteInspectAPI } from 'vite-plugin-inspect'
+// import type { ViteInspectAPI } from 'vite-plugin-inspect'
 import Inspect from 'vite-plugin-inspect'
 import type { ServerFunctions } from '../types'
 
@@ -8,18 +8,16 @@ export async function setup(nuxt: Nuxt, _functions: ServerFunctions) {
   const plugin = Inspect()
   addVitePlugin(plugin)
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  let api: ViteInspectAPI | undefined
+  // let _api: ViteInspectAPI | undefined
 
-  nuxt.hook('vite:serverCreated', () => {
-    api = plugin.api
-  })
+  // nuxt.hook('vite:serverCreated', () => {
+  //   _api = plugin.api
+  // })
 
   nuxt.hook('devtools:customTabs', (iframeTabs) => {
     iframeTabs.push({
       title: 'Inspect',
       name: 'vite-inspect',
-      builtin: true,
       icon: 'carbon-search',
       view: {
         type: 'iframe',
