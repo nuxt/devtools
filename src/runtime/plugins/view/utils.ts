f6efaaf6e4450ae6e64a9a40b909a40ce135cc42
Anthony Fu <anthonyfu117@hotmail.com>
2023-01-30 21:06:18 +0100

    fix: resize flickering

--- src/runtime/plugins/view/utils.ts
+++ src/runtime/plugins/view/utils.ts
@@ -1,6 +1,5 @@
 import type { Ref } from 'vue'
 import { computed, onScopeDispose, ref, watch } from 'vue'
-import { pauseTracking } from '@vue/reactivity'
 
 export function useObjectStorage<T>(key: string, initial: T): Ref<T> {
   const raw = localStorage.getItem(key)
@@ -11,16 +10,15 @@ export function useObjectStorage<T>(key: string, initial: T): Ref<T> {
       data.value[key] = initial[key]
   }
 
+  let wrote = ''
   watch(data, (value) => {
-    localStorage.setItem(key, JSON.stringify(value))
+    wrote = JSON.stringify(value)
+    localStorage.setItem(key, wrote)
   }, { deep: true })
 
   useEventListener(window, 'storage', (e: StorageEvent) => {
-    if (e.key === key && e.newValue) {
-      pauseTracking()
+    if (e.key === key && e.newValue && e.newValue !== wrote)
       data.value = JSON.parse(e.newValue)
-      resumeTracking()
-    }
   })
 
   return data
@@ -40,8 +38,4 @@ export function useTransform<F, T>(data: Ref<F>, to: (data: F) => T, from: (data
 export function useEventListener(target: EventTarget, type: string, listener: any, options?: boolean | AddEventListenerOptions) {
   target.addEventListener(type, listener, options)
   onScopeDispose(() => target.removeEventListener(type, listener, options))
-}
-
-function resumeTracking() {
-  throw new Error('Function not implemented.')
 }
