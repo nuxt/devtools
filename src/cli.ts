f88865e0b0f8b37108f16a219f78c82a883f19d4
Anthony Fu <anthonyfu117@hotmail.com>
2023-02-04 12:35:08 +0100

    chore: fix cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,9 @@
 import consola from 'consola'
 import { execa } from 'execa'
 import { read, update, write } from 'rc9'
+import { name } from '../package.json'
+
+const RC_PATH = '.nuxtrc'
 
 async function run() {
   const args = process.argv.slice(2)
@@ -8,17 +11,15 @@ async function run() {
 
   if (command === 'enable') {
     consola.info('Installed Nuxt Devtools...')
-    await execa('npm', ['install', '-g', '@nuxt/devtools@latest'])
-    update({
-      'modules[]': '@nuxt/devtools',
-    }, '.nuxtrc')
+    await execa('npm', ['install', '-g', `${name}@latest`])
+    update({ 'modules[]': name }, RC_PATH)
     consola.info('Nuxt Devtools enabled! Restart your Nuxt app to start using it.')
   }
   else if (command === 'disable') {
-    const rc = read('.nuxtrc')
-    if (rc.modules?.includes('@nuxt/devtools')) {
-      rc.modules = rc.modules.filter((m: string) => m !== '@nuxt/devtools')
-      write(rc, '.nuxtrc')
+    const rc = read(RC_PATH)
+    if (rc.modules?.includes(name)) {
+      rc.modules = rc.modules.filter((m: string) => m !== name)
+      write(rc, RC_PATH)
       consola.success('Nuxt Devtools disabled.')
     }
     else {
