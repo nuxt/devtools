4b9a33c17dce401459b257287e2b6598771370b4
Anthony Fu <anthonyfu117@hotmail.com>
2023-02-04 13:44:39 +0100

    fix: dir path

--- src/dirs.ts
+++ src/dirs.ts
@@ -3,5 +3,5 @@ import { fileURLToPath } from 'url'
 
 export const packageDir = resolve(fileURLToPath(import.meta.url), '../..')
 export const distDir = resolve(fileURLToPath(import.meta.url), '..')
-export const runtimeDir = resolve(distDir, '../runtime')
-export const clientDir = resolve(distDir, '../client')
+export const runtimeDir = resolve(distDir, 'runtime')
+export const clientDir = resolve(distDir, 'client')
