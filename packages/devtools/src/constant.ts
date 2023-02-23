0b42fde10d2bf30183ef00f104319c6ee8ff595a
Anthony Fu <anthonyfu117@hotmail.com>
2023-02-23 20:01:58 +0100

    feat: introduce local module entry

---
+++ packages/devtools/src/constant.ts
@@ -0,0 +1,15 @@
+import type { ModuleOptions } from '@nuxt/schema'
+
+export const ROUTE_PATH = '/__nuxt_devtools__'
+export const ROUTE_ENTRY = `${ROUTE_PATH}/entry`
+export const ROUTE_CLIENT = `${ROUTE_PATH}/client`
+
+export const defaultOptions: ModuleOptions = {
+  enabled: undefined,
+  vscode: {
+    enabled: true,
+    startOnBoot: false,
+    port: 3080,
+    reuseExistingServer: true,
+  },
+}
---
+++ packages/devtools/src/hooks.ts
@@ -0,0 +1,25 @@
+import type { ModuleCustomTab } from './types'
+
+declare module '@nuxt/schema' {
+  interface NuxtHooks {
+    /**
+     * Called before devtools starts. Useful to detect if devtools is enabled.
+     */
+    'devtools:before': () => void
+
+    /**
+     * Called after devtools is initialized.
+     */
+    'devtools:initialized': () => void
+
+    /**
+     * Hooks to extend devtools tabs.
+     */
+    'devtools:customTabs': (tabs: ModuleCustomTab[]) => void
+
+    /**
+     * Retrigger update for custom tabs, `devtools:customTabs` will be called again.
+     */
+    'devtools:customTabs:refresh': () => void
+  }
+}
