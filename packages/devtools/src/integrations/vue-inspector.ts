d608a0fed339f436bf83ff6e24807366b858f7d1
Anthony Fu <anthonyfu117@hotmail.com>
2023-03-18 11:46:35 +0100

    fix: component inspector, close #137

--- packages/devtools/src/integrations/vue-inspector.ts
+++ packages/devtools/src/integrations/vue-inspector.ts
@@ -8,7 +8,7 @@ export async function setup({ nuxt }: NuxtDevtoolsServerContext) {
     return
 
   addVitePlugin(VueInspector({
-    appendTo: 'entry.mjs',
+    appendTo: 'app/entry.js',
     toggleComboKey: '',
     toggleButtonVisibility: 'never',
   }) as Plugin)
