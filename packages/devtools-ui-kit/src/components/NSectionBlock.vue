0abc4b3cc931534431f4a1aa9134926814d489ce
Anthony Fu <anthonyfu117@hotmail.com>
2023-04-11 01:20:34 +0200

    feat: scroll trap in iframe

--- packages/devtools-ui-kit/src/components/NSectionBlock.vue
+++ packages/devtools-ui-kit/src/components/NSectionBlock.vue
@@ -47,7 +47,7 @@ function onToggle(e: any) {
         />
       </NIconTitle>
     </summary>
-    <div v-lazy-if="open" class="flex flex-col flex-gap2 pt2 pb6" :class="typeof padding === 'string' ? padding : padding ? 'px8' : ''">
+    <div v-lazy-show="open" class="flex flex-col flex-gap2 pt2 pb6" :class="typeof padding === 'string' ? padding : padding ? 'px8' : ''">
       <slot name="details" />
       <div :class="containerClass" class="mt1">
         <slot />
