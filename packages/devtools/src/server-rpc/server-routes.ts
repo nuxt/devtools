f1684f66583eaf18d4f4af75df0f772e99baa039
arashsheyda <sheidaeearash1999@gmail.com>
2023-04-25 19:46:48 +0300

    fix: server-routes watcher

--- packages/devtools/src/server-rpc/server-routes.ts
+++ packages/devtools/src/server-rpc/server-routes.ts
@@ -1,6 +1,7 @@
 import type { Nitro } from 'nitropack'
 import { join, resolve } from 'pathe'
 import fg from 'fast-glob'
+import { watch } from 'chokidar'
 import { withBase, withLeadingSlash, withoutTrailingSlash } from 'ufo'
 import type { NuxtDevtoolsServerContext, ServerFunctions, ServerRouteInfo } from '../types'
 
@@ -13,6 +14,26 @@ export function setupServerRoutesRPC({ nuxt, refresh }: NuxtDevtoolsServerContex
     nitro = _
     promiseCache = null
     refresh('getServerRoutes')
+
+    // Ported from Nitropack:
+    // https://github.com/unjs/nitro/blob/f49384d5540e10d97f3743670771aa5cb0b63d24/src/build.ts#L365
+    const watchReloadEvents = new Set(['add', 'addDir', 'unlink', 'unlinkDir'])
+    const watcher = watch([
+      join(nuxt.options.serverDir, 'api'),
+      join(nuxt.options.serverDir, 'routes'),
+    ], { ignoreInitial: true }).on(
+      'all',
+      (event) => {
+        if (watchReloadEvents.has(event)) {
+          promiseCache = null
+          refresh('getServerRoutes')
+        }
+      },
+    )
+
+    nitro.hooks.hook('close', () => {
+      watcher.close()
+    })
   })
 
   function scan() {
