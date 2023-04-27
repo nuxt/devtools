cd9d921d7a663a348a706846ae54d300ca76c61c
Anthony Fu <anthonyfu117@hotmail.com>
2023-04-27 15:11:50 +0200

    feat: refactor

--- packages/devtools/client/pages/modules/analyze-build.vue
+++ packages/devtools/client/pages/modules/analyze-build.vue
@@ -9,8 +9,6 @@ definePageMeta({
   category: 'analyze',
 })
 
-const ROUTE_ANALYZE = '/__nuxt_devtools__/analyze/'
-
 const PromiseConfirm = createTemplatePromise<boolean>()
 
 const info = useAnalyzeBuildInfo()
@@ -43,34 +41,8 @@ function gotoTerminal() {
     router.push(`/modules/terminals?id=${encodeURIComponent(processId.value)}`)
 }
 
-const tabs = computed(() => {
-  const items = [
-    { name: 'Overview', id: 'overview' },
-  ]
-  if (selected.value?.features.bundleClient)
-    items.push({ name: 'Client Bundle', id: 'bundle-client' })
-  if (selected.value?.features.bundleNitro)
-    items.push({ name: 'Nitro Bundle', id: 'bundle-nitro' })
-  if (selected.value?.features.viteInspect)
-    items.push({ name: 'Vite Inspect', id: 'vite-inspect' })
-
-  return items
-})
-
-const selectedTab = ref(tabs.value[0])
-
 function formatDuration(build: AnalyzeBuildMeta) {
   return `${((build.endTime - build.startTime) / 1000).toFixed(1)}s`
-}
-
-function formatFileSize(bytes: number) {
-  if (bytes < 1024)
-    return `${bytes}B`
-  if (bytes < 1024 * 1024)
-    return `${(bytes / 1024).toFixed(1)}KB`
-  if (bytes < 1024 * 1024 * 1024)
-    return `${(bytes / 1024 / 1024).toFixed(1)}MB`
-  return `${(bytes / 1024 / 1024 / 1024).toFixed(1)}GB`
 }
 </script>
 
@@ -105,92 +77,8 @@ function formatFileSize(bytes: number) {
       </div>
     </template>
     <template #right>
-      <div h-full grid="~ rows-[max-content_1fr]">
-        <div flex="~ wrap" w-full>
-          <template v-for="tab, idx of tabs" :key="idx">
-            <button
-              px4 py2 border="r base"
-              hover="bg-active"
-              :class="tab.id === selectedTab.id ? '' : 'border-b'"
-              @click="selectedTab = tab"
-            >
-              <div :class="tab.id === selectedTab.id ? '' : 'op30' ">
-                {{ tab.name }}
-              </div>
-            </button>
-          </template>
-          <div border="b base" flex-auto />
-        </div>
-        <div
-          v-if="selectedTab.id === 'overview'"
-          flex="~ col gap-4 items-center justify-center" p4
-        >
-          <div flex-auto />
-          <div v-if="selected" grid="~ cols-[30px_1fr] gap-x-2 gap-y-3 items-center justify-center" w-100>
-            <div i-carbon-commit text-xl />
-            <div>
-              <div text-sm op50>
-                Name
-              </div>
-              <div>{{ selected.name }}</div>
-            </div>
-            <div i-carbon-time text-xl />
-            <div>
-              <div text-sm op50>
-                Build duration
-              </div>
-              <div>{{ formatDuration(selected) }}</div>
-            </div>
-            <template v-if="selected.size?.clientBundle">
-              <div i-carbon-cics-program text-xl />
-              <div>
-                <div text-sm op50>
-                  Client bundle size
-                </div>
-                <div>{{ formatFileSize(selected.size.clientBundle) }}</div>
-              </div>
-            </template>
-            <template v-if="selected.size?.nitroBundle">
-              <div i-carbon-bare-metal-server text-xl />
-              <div>
-                <div text-sm op50>
-                  Nitro bundle size
-                </div>
-                <div>{{ formatFileSize(selected.size.nitroBundle) }}</div>
-              </div>
-            </template>
-            <div i-carbon-edge-node text-xl />
-            <div>
-              <div text-sm op50>
-                Built
-              </div>
-              <div>{{ formatTimeAgo(new Date(selected.endTime)) }}</div>
-            </div>
-          </div>
-          <NButton n="primary" icon="carbon-launch" @click="openInEditor(selected!.analyzeDir)">
-            Open in Editor
-          </NButton>
-          <div flex-auto />
-          <NButton n="rose" icon="carbon-delete" @click="rpc.clearAnalyzeBuilds([selected!.name])">
-            Delete this report
-          </NButton>
-        </div>
-        <iframe
-          v-lazy-show="selectedTab.id === 'bundle-client'"
-          :src="`${ROUTE_ANALYZE}${selected?.slug}/client.html`"
-          h-full w-full
-        />
-        <iframe
-          v-lazy-show="selectedTab.id === 'bundle-nitro'"
-          :src="`${ROUTE_ANALYZE}${selected?.slug}/nitro.html`"
-          h-full w-full
-        />
-        <iframe
-          v-lazy-show="selectedTab.id === 'vite-inspect'"
-          :src="`${ROUTE_ANALYZE}${selected?.slug}/.vite-inspect/`"
-          h-full w-full
-        />
-      </div>
+      <BuildAnalyzeDetails v-if="selected" :current="selected" />
+      <NPanelGrids v-else />
     </template>
   </PanelLeftRight>
 
