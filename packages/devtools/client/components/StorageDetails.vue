7af61bbcc823f9ee148cda16a6a8b70286f8c605
Anthony Fu <anthonyfu117@hotmail.com>
2023-06-15 21:06:26 +0200

    fix: require token for storage related operation

---
+++ packages/devtools/client/components/StorageDetails.vue
@@ -0,0 +1,218 @@
+<script setup lang="ts">
+import JsonEditorVue from 'json-editor-vue'
+
+const nuxtApp = useNuxtApp()
+const router = useRouter()
+const searchString = ref('')
+const newKey = ref('')
+const currentStorage = computed({
+  get(): string | undefined {
+    return useRoute().query?.storage as string | undefined
+  },
+  set(storage: string | undefined): void {
+    router.replace({ query: { storage } })
+  },
+})
+const currentItem = ref()
+const fileKey = computed(() => useRoute().query?.key as string | undefined)
+
+const { data: storageMounts } = await useAsyncData('storageMounts', () => rpc.getStorageMounts())
+const { data: storageKeys, refresh: refreshStorageKeys } = await useAsyncData('storageKeys', async () => {
+  if (currentStorage.value)
+    return await rpc.getStorageKeys(currentStorage.value)
+  return []
+})
+
+const closeWatcher = nuxtApp.hook('storage:key:update' as any, async (key: string, event: any) => {
+  if (!currentStorage.value || key.split(':')[0] !== currentStorage.value)
+    return
+  await refreshStorageKeys()
+  if (fileKey.value === key) {
+    if (event === 'remove')
+      return router.replace({ query: { storage: currentStorage.value } })
+    await fetchItem(fileKey.value)
+  }
+})
+
+onUnmounted(closeWatcher)
+
+watch(currentStorage, refreshStorageKeys)
+
+watchEffect(async () => {
+  if (!fileKey.value) {
+    currentItem.value = null
+    return
+  }
+  fetchItem(fileKey.value)
+})
+
+// Save on Ctrl/Cmd + S
+useEventListener('keydown', (e) => {
+  if (e.key === 's' && (e.ctrlKey || e.metaKey)) {
+    saveCurrentItem()
+    e.preventDefault()
+  }
+})
+
+function keyName(key: string) {
+  return key.replace(`${currentStorage.value}:`, '')
+}
+
+const filteredKeys = computed(() => {
+  if (!storageKeys.value)
+    return []
+  return storageKeys.value.filter(key => key.includes(searchString.value))
+})
+
+async function fetchItem(key: string) {
+  const content = await rpc.getStorageItem(await ensureDevAuthToken(), key)
+  currentItem.value = {
+    key,
+    updatedKey: keyName(key),
+    editingKey: false,
+    content,
+    updatedContent: content,
+  }
+}
+
+async function saveNewItem() {
+  if (!newKey.value || !currentStorage.value)
+    return
+  // If does not exists
+  const key = `${currentStorage.value}:${newKey.value}`
+  if (!storageKeys.value?.includes(key))
+    await rpc.setStorageItem(await ensureDevAuthToken(), key, '')
+
+  router.replace({ query: { storage: currentStorage.value, key } })
+  newKey.value = ''
+}
+
+async function saveCurrentItem() {
+  if (!currentItem.value)
+    return
+  await rpc.setStorageItem(await ensureDevAuthToken(), currentItem.value.key, currentItem.value.updatedContent)
+  await fetchItem(currentItem.value.key)
+}
+
+async function removeCurrentItem() {
+  if (!currentItem.value || !currentStorage.value)
+    return
+  await rpc.removeStorageItem(await ensureDevAuthToken(), currentItem.value.key)
+  currentItem.value = null
+}
+
+async function renameCurrentItem() {
+  if (!currentItem.value || !currentStorage.value)
+    return
+  const renamedKey = `${currentStorage.value}:${currentItem.value.updatedKey}`
+  const token = await ensureDevAuthToken()
+  await rpc.setStorageItem(token, renamedKey, currentItem.value.updatedContent)
+  await rpc.removeStorageItem(token, currentItem.value.key)
+  router.replace({ query: { storage: currentStorage.value, key: renamedKey } })
+}
+</script>
+
+<template>
+  <PanelLeftRight v-if="currentStorage" storage-key="tab-storage">
+    <template #left>
+      <div class="h-[48px] flex items-center justify-between gap1 px-3">
+        <NIconButton icon="carbon-chevron-left" ml--1 @click="currentStorage = ''" />
+        <div class="w-full text-sm">
+          <NSelect v-model="currentStorage" n="primary" icon="carbon-data-base">
+            <option v-for="(_storage, name) of storageMounts" :key="name" :value="name">
+              {{ name }}
+            </option>
+          </NSelect>
+        </div>
+      </div>
+      <NTextInput
+        v-model="searchString"
+        icon="carbon-search"
+        placeholder="Search..."
+        n="primary sm"
+        border="y x-none base! rounded-0"
+        class="w-full py2 ring-0!"
+      />
+      <template v-for="key of filteredKeys" :key="key">
+        <NuxtLink
+          block truncate px2 py1 font-mono text-sm
+          :to="{ query: { key, storage: currentStorage } }"
+          :class="key === currentItem?.key ? 'text-primary n-bg-active' : 'text-secondary hover:n-bg-hover'"
+        >
+          {{ keyName(key) }}
+        </NuxtLink>
+        <div x-divider />
+      </template>
+      <NTextInput
+        v-model="newKey"
+        icon="carbon-add"
+        placeholder="key"
+        n="sm"
+        border="t-none x-none base! rounded-0"
+        class="w-full py2 font-mono ring-0!"
+        @keyup.enter="saveNewItem"
+      />
+    </template>
+
+    <template #right>
+      <div v-if="currentItem?.key" h-full of-hidden flex="~ col">
+        <div border="b base" class="h-[49px] flex flex-none items-center justify-between px-4 text-sm">
+          <div class="flex items-center gap-4">
+            <NTextInput v-if="currentItem.editingKey" v-model="currentItem.updatedKey" @keyup.enter="renameCurrentItem" />
+            <code v-else>{{ keyName(currentItem.key) }} <NIcon icon="carbon-edit" class="cursor-pointer op50 hover:op100" @click="currentItem.editingKey = true" /></code>
+            <NButton v-if="!currentItem.editingKey" n="green xs" :disabled="currentItem.content === currentItem.updatedContent" :class="{ 'border-green': currentItem.content !== currentItem.updatedContent }" @click="saveCurrentItem">
+              Save
+            </NButton>
+          </div>
+          <div>
+            <NButton n="red xs" @click="removeCurrentItem">
+              Delete
+            </NButton>
+          </div>
+        </div>
+        <JsonEditorVue
+          v-if="typeof currentItem.content === 'object'"
+          v-model="currentItem.updatedContent"
+          :class="[$colorMode.value === 'dark' ? 'jse-theme-dark' : 'light']"
+          class="json-editor-vue h-full of-auto text-sm outline-none"
+          v-bind="$attrs" mode="text" :navigation-bar="false" :indentation="2" :tab-size="2"
+        />
+        <textarea
+          v-else v-model="currentItem.updatedContent"
+          placeholder="Item value..."
+          class="h-full of-auto p-4 font-mono text-sm outline-none"
+          @keyup.ctrl.enter="saveCurrentItem"
+        />
+      </div>
+      <NPanelGrids v-else>
+        <NCard px6 py4>
+          Select one key to start.<br>Learn more about <NLink href="https://nitro.unjs.io/guide/storage" n="orange" target="_blank">
+            Nitro storage
+          </NLink>
+        </NCard>
+      </NPanelGrids>
+    </template>
+  </PanelLeftRight>
+  <NPanelGrids v-else>
+    <p v-if="Object.keys(storageMounts as any).length" op50>
+      Select one storage to start:
+    </p>
+    <p v-else>
+      No custom storage defined in <code>nitro.storage</code>.<br>
+      Learn more about <NLink href="https://nitro.unjs.io/guide/storage" n="orange" target="_blank">
+        Nitro storage
+      </NLink>
+    </p>
+    <NCard
+      v-for="(storage, name) of storageMounts"
+      :key="name"
+      min-w-80 cursor-pointer p-4 text-left
+      hover="border-green"
+      @click="currentStorage = name as string"
+    >
+      <span font-bold>{{ name }}</span><br>
+      <span text-sm>{{ storage.driver }} driver</span><br>
+      <FilepathItem v-if="storage.base" text-xs :filepath="storage.base" />
+    </NCard>
+  </NPanelGrids>
+</template>
--- packages/devtools/client/pages/modules/custom-[name].vue
+++ packages/devtools/client/pages/modules/custom-[name].vue
@@ -43,25 +43,7 @@ onMounted(() => {
     </NPanelGrids>
   </template>
   <template v-if="tab.requireAuth && !isDevAuthed">
-    <NPanelGrids>
-      <NCard flex="~ col gap-2" mxa items-center p6 text-center>
-        <h3 class="mb2 text-lg font-medium leading-6" flex="~ items-center gap-1" text-orange>
-          <span class="i-carbon-information-square" /> Permissions required
-        </h3>
-        <p>
-          This operation requires permissions for running command and access files from the browser.
-        </p>
-        <p>
-          A request is sent to the server.<br>
-          Please check your terminal for the instructions and then come back.
-        </p>
-        <div flex="~ gap-3" mt2 justify-end>
-          <NButton disabled icon="i-carbon-time">
-            Waiting for authorization...
-          </NButton>
-        </div>
-      </NCard>
-    </NPanelGrids>
+    <AuthRequiredPanel />
   </template>
   <template v-else-if="tab.view.type === 'iframe'">
     <IframeView :tab="tab" />
