1f579b67a9028052d58cede7ca9213ead3d8bf28
Anthony Fu <anthonyfu117@hotmail.com>
2023-06-23 16:32:51 +0200

    feat: showing info

--- packages/devtools/client/components/DockingPanel.vue
+++ packages/devtools/client/components/DockingPanel.vue
@@ -15,23 +15,18 @@ function refreshPage() {
 <template>
   <div>
     <div px3 py2 border="b base" flex="~ gap-2">
-      <NButton v-if="client?.popup" n="sm primary" @click="client.popup()">
-        <div carbon-launch /> Popup
-      </NButton>
       <NDarkToggle v-slot="{ toggle, isDark }">
         <NButton n="sm primary" @click="toggle">
           <div carbon-sun dark:carbon-moon translate-y--1px /> {{ isDark.value ? 'Dark' : 'Light' }}
         </NButton>
       </NDarkToggle>
-      <NButton n="sm primary" to="/settings">
-        <div carbon-settings-adjust translate-y--1px /> Settings
-      </NButton>
+      <PictureInPictureButton />
     </div>
     <div px3 py2 flex="~ gap2">
-      <NButton n="solid primary xs" @click="refreshData">
+      <NButton n="primary sm" @click="refreshData">
         Refetch Data
       </NButton>
-      <NButton n="solid primary xs" @click="refreshPage">
+      <NButton n="primary sm" @click="refreshPage">
         Refresh Page
       </NButton>
     </div>
--- playgrounds/tab-seo/nuxt.config.ts
+++ playgrounds/tab-seo/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '../../packages/devtools-ui-kit',
+    '../../packages/devtools-ui-kit/src/module',
     '../../local',
   ],
 })
