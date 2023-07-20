bde6a6a5e298fa2bf4e8e6f952b364dfa6def42d
Arash <38922203+arashsheyda@users.noreply.github.com>
2023-07-20 13:18:32 +0300

    feat(assets): add watcher (#336)

--- packages/devtools/src/server-rpc/assets.ts
+++ packages/devtools/src/server-rpc/assets.ts
@@ -1,47 +1,69 @@
 import fsp from 'node:fs/promises'
 import { join, resolve } from 'pathe'
 import { imageMeta } from 'image-meta'
+import { debounce } from 'perfect-debounce'
 import fg from 'fast-glob'
-import type { AssetType, ImageMeta, NuxtDevtoolsServerContext, ServerFunctions } from '../types'
+import type { AssetInfo, AssetType, ImageMeta, NuxtDevtoolsServerContext, ServerFunctions } from '../types'
 
-export function setupAssetsRPC({ nuxt, ensureDevAuthToken }: NuxtDevtoolsServerContext) {
+export function setupAssetsRPC({ nuxt, ensureDevAuthToken, refresh }: NuxtDevtoolsServerContext) {
   const _imageMetaCache = new Map<string, ImageMeta | undefined>()
+  let cache: AssetInfo[] | null = null
+
+  const publicDir = resolve(nuxt.options.srcDir, nuxt.options.dir.public)
+
+  const refreshDebounced = debounce(() => {
+    cache = null
+    refresh('getStaticAssets')
+  }, 500)
+
+  nuxt.hook('builder:watch', (event, key) => {
+    if (key.startsWith(publicDir) && (event === 'add' || event === 'unlink'))
+      refreshDebounced()
+  })
+
+  async function scan() {
+    if (cache)
+      return cache
+
+    const baseURL = nuxt.options.app.baseURL
+    const files = await fg(['**/*'], {
+      cwd: publicDir,
+      onlyFiles: true,
+    })
+
+    function guessType(path: string): AssetType {
+      if (/\.(png|jpe?g|jxl|gif|svg|webp|avif|ico|bmp|tiff?)$/i.test(path))
+        return 'image'
+      if (/\.(mp4|webm|ogv|mov|avi|flv|wmv|mpg|mpeg|mkv|3gp|3g2|ts|mts|m2ts|vob|ogm|ogx|rm|rmvb|asf|amv|divx|m4v|svi|viv|f4v|f4p|f4a|f4b)$/i.test(path))
+        return 'video'
+      if (/\.(mp3|wav|ogg|flac|aac|wma|alac|ape|ac3|dts|tta|opus|amr|aiff|au|mid|midi|ra|rm|wv|weba|dss|spx|vox|tak|dsf|dff|dsd|cda)$/i.test(path))
+        return 'audio'
+      if (/\.(woff2?|eot|ttf|otf|ttc|pfa|pfb|pfm|afm)/i.test(path))
+        return 'font'
+      if (/\.(json[5c]?|te?xt|[mc]?[jt]sx?|md[cx]?|markdown)/i.test(path))
+        return 'text'
+      return 'other'
+    }
+
+    cache = await Promise.all(files.map(async (path) => {
+      const filePath = resolve(publicDir, path)
+      const stat = await fsp.lstat(filePath)
+      return {
+        path,
+        publicPath: join(baseURL, path),
+        filePath,
+        type: guessType(path),
+        size: stat.size,
+        mtime: stat.mtimeMs,
+      }
+    }))
+
+    return cache
+  }
 
   return {
     async getStaticAssets() {
-      const dir = resolve(nuxt.options.srcDir, nuxt.options.dir.public)
-      const baseURL = nuxt.options.app.baseURL
-      const files = await fg(['**/*'], {
-        cwd: dir,
-        onlyFiles: true,
-      })
-
-      function guessType(path: string): AssetType {
-        if (/\.(png|jpe?g|jxl|gif|svg|webp|avif|ico|bmp|tiff?)$/i.test(path))
-          return 'image'
-        if (/\.(mp4|webm|ogv|mov|avi|flv|wmv|mpg|mpeg|mkv|3gp|3g2|ts|mts|m2ts|vob|ogm|ogx|rm|rmvb|asf|amv|divx|m4v|svi|viv|f4v|f4p|f4a|f4b)$/i.test(path))
-          return 'video'
-        if (/\.(mp3|wav|ogg|flac|aac|wma|alac|ape|ac3|dts|tta|opus|amr|aiff|au|mid|midi|ra|rm|wv|weba|dss|spx|vox|tak|dsf|dff|dsd|cda)$/i.test(path))
-          return 'audio'
-        if (/\.(woff2?|eot|ttf|otf|ttc|pfa|pfb|pfm|afm)/i.test(path))
-          return 'font'
-        if (/\.(json[5c]?|te?xt|[mc]?[jt]sx?|md[cx]?|markdown)/i.test(path))
-          return 'text'
-        return 'other'
-      }
-
-      return await Promise.all(files.map(async (path) => {
-        const filePath = resolve(dir, path)
-        const stat = await fsp.lstat(filePath)
-        return {
-          path,
-          publicPath: join(baseURL, path),
-          filePath,
-          type: guessType(path),
-          size: stat.size,
-          mtime: stat.mtimeMs,
-        }
-      }))
+      return await scan()
     },
     async getImageMeta(filepath: string) {
       if (_imageMetaCache.has(filepath))
