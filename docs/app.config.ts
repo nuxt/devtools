d981f1c35506053e41b51f9cd96045e51f7c6836
Florent Delerue <florentdelerue@hotmail.com>
2023-09-05 18:17:38 +0200

    feat(landing): wip

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -2,6 +2,14 @@ export default defineAppConfig({
   ui: {
     primary: 'green',
     gray: 'slate',
+
+    button: {
+      color: {
+        white: {
+          link: 'text-white dark:text-white hover:text-gray-300 dark:hover:text-gray-300 underline-offset-4 hover:underline focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-gray-500 dark:focus-visible:ring-gray-400 transition-all duration-200',
+        },
+      },
+    },
   },
   elements: {
     variables: {
--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -2,8 +2,8 @@
 definePageMeta({
   colorMode: 'dark',
 })
-const title = 'Nuxt Image: Optimized Images for your Nuxt Apps'
-const description = 'Plug-and-play image optimization for Nuxt apps. Resize and transform your images using built-in optimizer or your favorite images CDN.'
+const title = 'Nuxt DevTool: Unleash Nuxt Developer Experience'
+const description = 'Nuxt DevTools: Elevate your Nuxt App insight and Developer Experience. Enhance transparency, identify performance gaps, and seamlessly manage app configurations.'
 useSeoMeta({
   titleTemplate: '',
   title,
@@ -13,20 +13,103 @@ useSeoMeta({
   ogImage: 'https://image.nuxt.com/social-card.png',
   twitterImage: 'https://image.nuxt.com/social-card.png',
 })
-const source = ref('npm i @nuxt/image@rc')
+
+const advancedCards = [
+  {
+    title: 'Developer experience',
+    description: 'Enhance your DX even further, and adding an extra layer of enjoyment to the development journey!',
+    icon: 'i-ph-code-light',
+    to: ''
+  },
+  {
+    title: 'Inspection & debug',
+    description: 'See the relationships between your components, composables, modules and so much more.',
+    icon: 'i-ph-magnifying-glass',
+    to: ''
+  },
+  {
+    title: 'Transparency conventions',
+    description: 'Understand your Nuxt app structure to debug and optimize your Nuxt application effectively.',
+    icon: 'i-ph-wall',
+    to: ''
+  },
+  {
+    title: 'Real-time insights',
+    description: 'Get real-time insights into your app\'s performance. Analyze your data for seamless user experience.',
+    icon: 'i-ph-chart-bar',
+    to: ''
+  },
+  {
+    title: 'Tailor-made documentation',
+    description: 'Get personalized documentation for your Nuxt application, based on your usage patterns and needs.',
+    icon: 'i-ph-book-open',
+    to: ''
+  },
+  {
+    title: 'Extendable & hackable',
+    description: 'Provide interactive views for integrations. Making it easy to explore and understand your application\'s data and performance.',
+    icon: 'i-ph-plug',
+    to: ''
+  },
+]
+
+const projectsCard = [
+  {
+    title: 'Pages',
+    description: 'Access your app\'s routes interactively and use the text box to read and test how each route matches the Pages tab.',
+    to: '#'
+  },
+  {
+    title: 'Components',
+    description: 'Explore your app\'s components interactively in the Components tab. Easily search and view their source code.',
+    to: '#'
+  },
+  {
+    title: 'Imports',
+    description: 'Interactively explore Nuxt auto-imports in the Imports tab. See all files, sources, and optional descriptions and documentations.',
+    to: '#'
+  },
+]
+
+const source = ref('npx nuxi@latest devtools enable')
 const { copy, copied } = useClipboard({ source })
 
-const providers = ['cloudflare', 'cloudimage', 'cloudinary', 'directus', 'edgio', 'fastly', 'glide', 'gumlet', 'imageengine', 'imagekit', 'imgix', 'ipx', 'netlify', 'prepr', 'prismic', 'sanity', 'storyblok', 'strapi', 'twicpics', 'unsplash', 'uploadcare', 'vercel']
-// Disabling because svg to png does not work now with SSG
-// Related issue: https://github.com/unjs/ipx/issues/160
-// const img = useImage()
+const intervalId = ref()
+const currentStep = ref(0)
+const projectsSectionVisible = ref(false)
+const projectsSection = ref(null)
+
+const selectProjectCard = (index) => {
+  currentStep.value = index
+
+  clearInterval(intervalId.value)
+}
+
+const { stop } = useIntersectionObserver(
+  projectsSection,
+  ([{ isIntersecting }], observerElement) => {
+    projectsSectionVisible.value = isIntersecting
+  },
+)
+
+watch(projectsSectionVisible, () => {
+  if (projectsSectionVisible.value) {
+    intervalId.value = setInterval(() => {
+      if (currentStep.value < 2) {
+        currentStep.value++
+      } else {
+        currentStep.value = 0
+      }
+    }, 4000)
+  }
+})
+
+
 </script>
 
 <template>
-  <ULandingHero
-    align="center" direction="vertical"
-    :ui="{ container: 'flex lg:gap-12', description: 'mt-6 text-lg/8 lg:px-28 text-gray-400' }"
-  >
+  <ULandingHero align="center" direction="vertical"
+    :ui="{ container: 'flex lg:gap-12', description: 'mt-6 text-lg/8 lg:px-28 text-gray-400' }">
     <span class="gradient" />
     <template #title>
       Unleash Nuxt <br><span class="text-primary-400">Developer Experience</span>
@@ -39,22 +122,19 @@ const providers = ['cloudflare', 'cloudimage', 'cloudinary', 'directus', 'edgio'
       <UButton to="/get-started/installation" icon="i-ph-rocket-launch-duotone" size="xl">
         Get started
       </UButton>
-      <UInput
-        aria-label="Copy code to get started" :model-value="source" name="get-started" disabled autocomplete="off"
-        size="xl" :ui="{ base: 'disabled:cursor-default', icon: { trailing: { pointer: '' } } }"
-      >
+      <UInput aria-label="Copy code to get started" :model-value="source" name="get-started" disabled autocomplete="off"
+        size="xl" :ui="{ base: 'disabled:cursor-default', icon: { trailing: { pointer: '' } } }">
         <template #leading>
           <UIcon name="i-ph-terminal" />
         </template>
         <template #trailing>
-          <UButton
-            aria-label="Copy Code" :color="copied ? 'green' : 'gray'" variant="ghost" :padded="false"
-            :icon="copied ? 'i-ph-check-square-duotone' : 'i-ph-copy-duotone'" @click="copy(source)"
-          />
+          <UButton aria-label="Copy Code" :color="copied ? 'green' : 'gray'" variant="ghost" :padded="false"
+            :icon="copied ? 'i-ph-check-square-duotone' : 'i-ph-copy-duotone'" @click="copy(source)" />
         </template>
       </UInput>
     </template>
   </ULandingHero>
+
   <UContainer class="flex flex-col items-center justify-center">
     <VideoPlayer :source="{ type: 'mp4', src: '/videos/nuxt.mp4' }" poster="/videos/poster-volta.webp" />
     <span class="pt-4">Discover Nuxt DevTools in 3 minutes ✨</span>
@@ -62,60 +142,64 @@ const providers = ['cloudflare', 'cloudimage', 'cloudinary', 'directus', 'edgio'
 
   <ULandingSection>
     <template #title>
-      Get the most of your images with<br><span class="text-primary-400">dynamic features</span>
+      Enhance projects with<br><span class="text-primary-400">Advanced DevTools</span>
     </template>
     <UPageGrid>
-      <!-- <UPageCard to="/usage/nuxt-img" icon="i-ph-image-duotone">
-        <template #title>
-          Drop-in Replacement
-        </template>
-        Leverage <code>&lt;nuxt-img&gt;</code> and <code>&lt;nuxt-picture&gt;</code> drop-in replacement for the native
-        <code>&lt;img&gt;</code> and <code>&lt;picture&gt;</code> elements.
+      <UPageCard v-for="card in advancedCards" :key="card.title" :to="card.to" :icon="card.icon" :title="card.title"
+        :description="card.description"
+        :ui="{ to: 'hover:ring-2 dark:hover:ring-gray-500 hover:ring-gray-500 hover:bg-gray-100/50', icon: { base: 'w-10 h-10 flex-shrink-0 text-gray-100' }, body: { base: 'h-full', background: 'bg-gradient-to-b from-gray-900 to-gray-950' } }">
       </UPageCard>
-      <UPageCard to="/providers/ipx" title="Built-in resizer" icon="i-ph-crop">
-        Unlock the power of our built-in image resizer and transformer with IPX.
-      </UPageCard>
-      <UPageCard to="/get-started/providers" title="20+ Providers Supported" icon="i-ph-plug-duotone">
-        Choose over 20 image providers to optimize your images, no vendor lock-in.
-      </UPageCard>
-      <UPageCard to="/usage/nuxt-img#sizes" title="Responsive Ready" icon="i-ph-frame-corners">
-        Nuxt Image dynamically generates responsive sizes. Your visuals adapt effortlessly to every screen size, offering
-        a seamless user experience.
-      </UPageCard>
-      <UPageCard to="/usage/nuxt-img#format" title="Future-Proof Formats" icon="i-ph-magic-wand-duotone">
-        Embrace the future of visual optimization with support for modern formats like webp and avif.
-      </UPageCard>
-      <UPageCard to="/get-started/installation" title="Streamlined Performance" icon="i-ph-rocket-launch-duotone">
-        Deliver visually stunning content with lightning-fast load times, keeping your audience engaged and delighted.
-      </UPageCard> -->
     </UPageGrid>
   </ULandingSection>
-  <ULandingSection align="left">
+
+  <ULandingSection align="center">
     <template #title>
-      Choose your<br><span class="text-primary-400">favorite provider</span>
+      Explore the depths of your<br><span class="text-primary-400">Nuxt project</span>
     </template>
-    <template #description>
-      Nuxt Image supports multiple providers for high performance.<br>
-      Providers are integrations between Nuxt Image and third-party image transformation services. Each provider is
-      responsible for generating correct URLs for that image transformation service. Nuxt Image can also be configured to
-      work with any external image transformation service.
-    </template>
-    <template #links>
-      <UButton to="/get-started/installation" icon="i-ph-rocket-launch-duotone" size="xl">
-        Get Started
-      </UButton>
-    </template>
-    <div class="grid grid-cols-5 gap-4 lg:grid-cols-6 md:grid-cols-8 sm:grid-cols-6 lg:gap-6 sm:gap-5">
-      <NuxtLink
-        v-for="(provider, index) in providers" :key="index" :to="`/providers/${provider}`"
-        class="block transition lg:hover:scale-110"
-      >
-        <!-- <NuxtImg :placeholder="img(`/providers/${provider}.svg`, { h: 10, f: 'png', blur: 2, q: 50 })" :src="`/providers/${provider}.svg`" :alt="provider" width="64" height="64" class="w-12 h-12 sm:w-16 sm:h-16 rounded-xl" loading="lazy" /> -->
-        <NuxtImg
-          :src="`/providers/${provider}.svg`" :alt="provider" width="64" height="64"
-          class="h-12 w-12 rounded-xl sm:h-16 sm:w-16" loading="lazy"
-        />
-      </NuxtLink>
+    <div class="flex flex-row gap-x-12" ref="projectsSection">
+      <ul class="lg:w-[40%] flex flex-col items-center justify-center">
+        <li v-for="(project, index) in  projectsCard" :key="index">
+          <UCard class="hidden lg:block relative"
+            :ui="{ background: 'bg-transparent dark:bg-transparent', sahdow: 'none', ring: 'ring-0', body: { background: 'bg-transparent dark:bg-transparent', base: 'flex flex-col space-y-2' } }">
+            <div class="absolute inset-0 w-full h-full" @click="selectProjectCard(index)" />
+            <h4 class="text-xl font-medium" :class="currentStep === index ? 'text-white' : 'text-gray-400'">
+              {{ project.title }}
+            </h4>
+            <p :class="currentStep === index ? 'text-gray-400' : 'text-gray-600'">
+              {{ project.description }}
+            </p>
+            <UButton :to="project.to" trailingIcon="i-ph-arrow-right" variant="link" color="white" size="md"
+              :ui="{ size: { md: 'text-md' } }" class="-ml-2.5">
+              <span :class="currentStep === index ? 'text-white' : 'text-gray-400'">Learn more</span>
+            </UButton>
+          </UCard>
+
+          <ULandingSection align="center"
+            :icon="index === 0 ? 'i-ph-tree-structure' : index === 1 ? 'i-ph-circles-three' : 'i-ph-function'"
+            class="lg:hidden"
+            :ui="{ base: 'flex flex-col items-center', wrapper: 'py-8 sm:py-12', icon: { wrapper: 'relative rounded-lg flex items-center justify-center mb-6 w-10 h-10 bg-gray-700 flex-shrink-0' }, title: 'text-2xl font-bold tracking-tight text-white sm:text-3xl lg:text-4xl' }">
+            <template #title>
+              {{ project.title }}
+            </template>
+            <template #description>
+              {{ project.description }}
+            </template>
+            <div
+              class="lg:hidden lg:w-[60%] items-center justify-center border border-slate-200/10 rounded-xl bg-slate-700/20 h-full place-self-center">
+              <div class="p-4">
+                <NuxtImg :src="`/images/${index === 0 ? 'pages' : index === 1 ? 'components' : 'imports'}.webp`" />
+              </div>
+            </div>
+          </ULandingSection>
+
+        </li>
+      </ul>
+      <div
+        class="hidden lg:flex lg:w-[60%] items-center justify-center border border-slate-200/10 rounded-xl bg-slate-700/20 h-full place-self-center">
+        <div class="p-4">
+          <NuxtImg :src="`/images/${currentStep === 0 ? 'pages' : currentStep === 1 ? 'components' : 'imports'}.webp`" />
+        </div>
+      </div>
     </div>
   </ULandingSection>
 </template>
