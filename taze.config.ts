cb0f0250c7b1839554eaa267151835ad62e3726d
Anthony Fu <anthonyfu117@hotmail.com>
2023-09-06 10:37:54 +0200

    chore: update deps

--- taze.config.ts
+++ taze.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'taze'
 export default defineConfig({
   exclude: [
     'execa', // Node compatibility,
-    'unbuild', // Waiting for https://github.com/nuxt/module-builder/pull/161
+    'which', // Node compatibility,
   ],
   ignorePaths: [
     'clones',
