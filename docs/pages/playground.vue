8a4a16347d20a2ec29e33cbbefa647138d625053
Sébastien Chopin <seb@nuxt.com>
2023-09-11 16:17:47 +0200

    docs: update playground

--- docs/pages/playground.vue
+++ docs/pages/playground.vue
@@ -3,23 +3,20 @@ const title = 'Playground'
 const description = 'Play online with our interactive Nuxt DevTools playground.'
 useSeoMeta({
   title,
-  ogTitle: 'Nuxt DevTools Playground',
-  description,
+  ogTitle: `Nuxt DevTools Playground`,
+  description
 })
 defineOgImage({
   component: 'Docs',
   title,
-  description,
+  description
 })
 </script>
 
 <template>
   <div class="h-[calc(100vh-var(--header-height))] flex dark:bg-gray-950">
     <ClientOnly>
-      <!-- TODO: add devtools playground links -->
-      <iframe class="dark:bg-gray-950"
-        :src="`https://stackblitz.com/github/nuxt/image/tree/main/example?embed=1&file=app.vue&theme=${$colorMode.preference}`"
-        width="100%" height="100%" />
+      <iframe class="dark:bg-gray-950" :src="`https://stackblitz.com/github/nuxt/starter/tree/v3?embed=1&file=app.vue&theme=${$colorMode.preference}`" width="100%" height="100%" />
     </ClientOnly>
   </div>
 </template>
