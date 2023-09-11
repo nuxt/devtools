b5b34c7f6a5002ff96810c71e112aacd7ddf135b
Florent Delerue <florentdelerue@hotmail.com>
2023-09-11 15:34:07 +0200

    fix: projects section

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -25,8 +25,6 @@ const intervalId = ref()
 const currentStep = ref(0)
 const projectsSectionVisible = ref(false)
 const nuxtProjectsSection = ref(null)
-const progressBarMaxDuration = 8000
-const progressBarCounter = ref(0)
 
 onMounted(() => console.log({ nuxtProjectsSection }))
 
@@ -42,7 +40,7 @@ const { data: module } = await useFetch<{
   transform: ({ stats, contributors }) => ({ stats, contributors }),
 })
 
-function selectProjectCard(index) {
+function selectProjectCard (index) {
   currentStep.value = index
 
   clearInterval(intervalId.value)
@@ -56,13 +54,24 @@ const { stop } = useIntersectionObserver(
 )
 
 const { format: formatNumber } = Intl.NumberFormat('en-GB', { notation: 'compact' })
+
+
+watch(projectsSectionVisible, () => {
+  if (projectsSectionVisible.value) {
+    intervalId.value = setInterval(() => {
+      if (currentStep.value < 2) {
+        currentStep.value++
+      } else {
+        currentStep.value = 0
+      }
+    }, 4000)
+  }
+})
 </script>
 
 <template>
-  <ULandingHero
-    align="center" direction="vertical"
-    :ui="{ container: 'flex flex-col lg:gap-12', description: 'mt-6 text-lg/8 lg:px-28 text-gray-400' }"
-  >
+  <ULandingHero align="center" direction="vertical"
+    :ui="{ container: 'flex flex-col lg:gap-12', description: 'mt-6 text-lg/8 lg:px-28 text-gray-400' }">
     <span class="gradient" />
     <template #title>
       <span v-html="page.hero?.title" />
@@ -74,18 +83,14 @@ const { format: formatNumber } = Intl.NumberFormat('en-GB', { notation: 'compact
       <UButton to="/guide" icon="i-ph-rocket-launch-duotone" size="xl">
         {{ page.hero?.button }}
       </UButton>
-      <UInput
-        aria-label="Copy code to get started" :model-value="source" name="get-started" disabled autocomplete="off"
-        size="xl" :ui="{ base: 'disabled:cursor-default', icon: { trailing: { pointer: '' } } }"
-      >
+      <UInput aria-label="Copy code to get started" :model-value="source" name="get-started" disabled autocomplete="off"
+        size="xl" :ui="{ base: 'disabled:cursor-default', icon: { trailing: { pointer: '' } } }">
         <template #leading>
           <UIcon name="i-ph-terminal" />
         </template>
         <template #trailing>
-          <UButton
-            aria-label="Copy Code" :color="copied ? 'green' : 'gray'" variant="ghost" :padded="false"
-            :icon="copied ? 'i-ph-check-square-duotone' : 'i-ph-copy-duotone'" @click="copy(source)"
-          />
+          <UButton aria-label="Copy Code" :color="copied ? 'green' : 'gray'" variant="ghost" :padded="false"
+            :icon="copied ? 'i-ph-check-square-duotone' : 'i-ph-copy-duotone'" @click="copy(source)" />
         </template>
       </UInput>
     </template>
@@ -108,11 +113,9 @@ const { format: formatNumber } = Intl.NumberFormat('en-GB', { notation: 'compact
 
     <template #tools>
       <UPageGrid>
-        <UPageCard
-          v-for="card in section.toolsCards" :key="card.title" :to="card.to" :icon="card.icon"
+        <UPageCard v-for="card in section.toolsCards" :key="card.title" :to="card.to" :icon="card.icon"
           :title="card.title" :description="card.description"
-          :ui="{ to: 'hover:ring-2 dark:hover:ring-gray-500 hover:ring-gray-500 hover:bg-gray-100/50', icon: { base: 'w-10 h-10 flex-shrink-0 text-gray-100' }, body: { base: 'h-full', background: 'bg-gradient-to-b from-gray-900 to-gray-950' } }"
-        />
+          :ui="{ to: 'hover:ring-2 dark:hover:ring-gray-500 hover:ring-gray-500 hover:bg-gray-100/50', icon: { base: 'w-10 h-10 flex-shrink-0 text-gray-100' }, body: { base: 'h-full', background: 'bg-gradient-to-b from-gray-900 to-gray-950' } }" />
       </UPageGrid>
     </template>
 
@@ -120,10 +123,8 @@ const { format: formatNumber } = Intl.NumberFormat('en-GB', { notation: 'compact
       <div ref="nuxtProjectsSection" class="flex flex-row gap-x-12">
         <ul class="flex flex-col items-center justify-center lg:w-[40%]">
           <li v-for="(project, index) in section.projectCards" :key="index">
-            <UCard
-              class="relative hidden lg:block"
-              :ui="{ background: 'bg-transparent dark:bg-transparent', sahdow: 'none', ring: 'ring-0', body: { background: 'bg-transparent dark:bg-transparent', base: 'flex flex-col space-y-2' } }"
-            >
+            <UCard class="relative hidden lg:block cursor-pointer"
+              :ui="{ background: 'bg-transparent dark:bg-transparent', sahdow: 'none', ring: 'ring-0', body: { background: 'bg-transparent dark:bg-transparent', base: 'flex flex-col space-y-2' } }">
               <div class="absolute inset-0 h-full w-full" @click="selectProjectCard(index)" />
               <h4 class="text-xl font-medium" :class="currentStep === index ? 'text-white' : 'text-gray-400'">
                 {{ project.title }}
@@ -131,20 +132,16 @@ const { format: formatNumber } = Intl.NumberFormat('en-GB', { notation: 'compact
               <p :class="currentStep === index ? 'text-gray-400' : 'text-gray-600'">
                 {{ project.description }}
               </p>
-              <UButton
-                :to="project.to" trailing-icon="i-ph-arrow-right" variant="link" color="white" size="md"
-                :ui="{ size: { md: 'text-md' } }" class="-ml-2.5"
-              >
+              <UButton :to="project.to" trailing-icon="i-ph-arrow-right" variant="link" color="white" size="md"
+                :ui="{ size: { md: 'text-md' } }" class="-ml-2.5">
                 <span :class="currentStep === index ? 'text-white' : 'text-gray-400'">Learn more</span>
               </UButton>
             </UCard>
 
-            <ULandingSection
-              align="center"
+            <ULandingSection align="center"
               :icon="index === 0 ? 'i-ph-tree-structure' : index === 1 ? 'i-ph-circles-three' : 'i-ph-function'"
               class="lg:hidden"
-              :ui="{ base: 'flex flex-col items-center', wrapper: 'py-8 sm:py-12', icon: { wrapper: 'relative rounded-lg flex items-center justify-center mb-6 w-10 h-10 bg-gray-700 flex-shrink-0' }, title: 'text-2xl font-bold tracking-tight text-white sm:text-3xl lg:text-4xl' }"
-            >
+              :ui="{ base: 'flex flex-col items-center', wrapper: 'py-8 sm:py-12', icon: { wrapper: 'relative rounded-lg flex items-center justify-center mb-6 w-10 h-10 bg-gray-700 flex-shrink-0' }, title: 'text-2xl font-bold tracking-tight text-white sm:text-3xl lg:text-4xl' }">
               <template #title>
                 {{ project.title }}
               </template>
@@ -152,8 +149,7 @@ const { format: formatNumber } = Intl.NumberFormat('en-GB', { notation: 'compact
                 {{ project.description }}
               </template>
               <div
-                class="relative h-full place-self-center items-center justify-center border border-slate-200/10 rounded-xl bg-slate-700/20 lg:hidden"
-              >
+                class="relative h-full place-self-center items-center justify-center border border-slate-200/10 rounded-xl bg-slate-700/20 lg:hidden">
                 <div class="p-4">
                   <NuxtImg :src="`/images/${index === 0 ? 'pages' : index === 1 ? 'components' : 'imports'}.webp`" />
                 </div>
@@ -162,46 +158,36 @@ const { format: formatNumber } = Intl.NumberFormat('en-GB', { notation: 'compact
           </li>
         </ul>
         <div
-          class="relative hidden h-full place-self-center items-center justify-center border border-slate-200/10 rounded-xl bg-slate-700/20 lg:w-[60%] lg:flex"
-        >
+          class="relative hidden h-full place-self-center items-center justify-center border border-slate-200/10 rounded-xl bg-slate-700/20 lg:w-[60%] lg:flex">
           <div class="p-4">
             <NuxtImg
-              :src="`/images/${currentStep === 0 ? 'pages' : currentStep === 1 ? 'components' : 'imports'}.webp`"
-            />
+              :src="`/images/${currentStep === 0 ? 'pages' : currentStep === 1 ? 'components' : 'imports'}.webp`" />
           </div>
         </div>
       </div>
     </template>
 
     <template #cta>
-      <ULandingCTA
-        align="left" card :ui="{
-          background: 'bg-gradient-to-b from-gray-900 to-gray-950',
-          body: { background: 'bg-gradient-to-b from-gray-900 to-gray-950' },
-          links: 'mt-10 flex flex-col space-y-4 items-center justify-center lg:justify-start gap-x-6',
-          title: 'text-2xl font-medium tracking-tight text-white sm:text-3xl text-center lg:text-left',
-        }"
-      >
+      <ULandingCTA align="left" card :ui="{
+        background: 'bg-gradient-to-b from-gray-900 to-gray-950',
+        body: { background: 'bg-gradient-to-b from-gray-900 to-gray-950' },
+        links: 'mt-10 flex flex-col space-y-4 items-center justify-center lg:justify-start gap-x-6',
+        title: 'text-2xl font-medium tracking-tight text-white sm:text-3xl text-center lg:text-left',
+      }">
         <template #title>
           <span v-html="section.title" />
         </template>
 
         <template #links>
           <UAvatarGroup :max="13" size="md" class="flex-wrap lg:self-start [&_span:first-child]:text-xs">
-            <UTooltip
-              v-for="(contributor, index) of module.contributors" :key="index" :text="contributor.username"
+            <UTooltip v-for="(contributor, index) of module.contributors" :key="index" :text="contributor.username"
               class="rounded-full" :ui="{ background: 'bg-gray-50 dark:bg-gray-800/50' }"
-              :popper="{ offsetDistance: 16 }"
-            >
-              <UAvatar
-                :alt="contributor.username" :src="`https://github.com/${contributor.username}.png`"
+              :popper="{ offsetDistance: 16 }">
+              <UAvatar :alt="contributor.username" :src="`https://github.com/${contributor.username}.png`"
                 class="lg:hover:ring-primary-500 dark:lg:hover:ring-primary-400 transition-transform lg:hover:scale-125 lg:hover:ring-2"
-                size="md"
-              >
-                <NuxtLink
-                  :to="`https://github.com/${contributor.username}`" target="_blank" class="focus:outline-none"
-                  tabindex="-1"
-                >
+                size="md">
+                <NuxtLink :to="`https://github.com/${contributor.username}`" target="_blank" class="focus:outline-none"
+                  tabindex="-1">
                   <span class="absolute inset-0" aria-hidden="true" />
                 </NuxtLink>
               </UAvatar>
@@ -215,8 +201,7 @@ const { format: formatNumber } = Intl.NumberFormat('en-GB', { notation: 'compact
         <div class="flex flex-col items-center justify-center gap-8 sm:flex-row lg:gap-16">
           <NuxtLink class="group text-center" to="https://npmjs.org/package/@nuxt/devtools" target="_blank">
             <p
-              class="group-hover:text-primary-500 dark:group-hover:text-primary-400 text-6xl font-semibold text-gray-900 dark:text-white"
-            >
+              class="group-hover:text-primary-500 dark:group-hover:text-primary-400 text-6xl font-semibold text-gray-900 dark:text-white">
               {{ formatNumber(module.stats.downloads) }}+
             </p>
             <p>Monthly Downloads</p>
@@ -224,8 +209,7 @@ const { format: formatNumber } = Intl.NumberFormat('en-GB', { notation: 'compact
 
           <NuxtLink class="group text-center" to="https://github.com/nuxt/devtools" target="_blank">
             <p
-              class="group-hover:text-primary-500 dark:group-hover:text-primary-400 text-6xl font-semibold text-gray-900 dark:text-white"
-            >
+              class="group-hover:text-primary-500 dark:group-hover:text-primary-400 text-6xl font-semibold text-gray-900 dark:text-white">
               {{ formatNumber(module.stats.stars) }}+
             </p>
             <p>Stars</p>
@@ -239,20 +223,16 @@ const { format: formatNumber } = Intl.NumberFormat('en-GB', { notation: 'compact
         <div class="flex flex-col space-y-6">
           <div class="flex space-x-4">
             <div class="relative hidden flex-col justify-between py-[20px] md:flex">
-              <svg
-                width="1" height="154" viewBox="0 0 1 154" fill="none" xmlns="http://www.w3.org/2000/svg"
-                class="absolute left-4 z-[-1]"
-              >
+              <svg width="1" height="154" viewBox="0 0 1 154" fill="none" xmlns="http://www.w3.org/2000/svg"
+                class="absolute left-4 z-[-1]">
                 <path d="M0.500244 0.568115L0.500244 153.568" stroke="#334155" stroke-dasharray="4 4" />
               </svg>
               <div
-                class="h-8 w-8 flex items-center justify-center border border-1 border-gray-700 rounded-full bg-gray-800 px-4 py-2"
-              >
+                class="h-8 w-8 flex items-center justify-center border border-1 border-gray-700 rounded-full bg-gray-800 px-4 py-2">
                 1
               </div>
               <div
-                class="h-8 w-8 flex items-center justify-center border border-1 border-gray-700 rounded-full bg-gray-800 px-4 py-2"
-              >
+                class="h-8 w-8 flex items-center justify-center border border-1 border-gray-700 rounded-full bg-gray-800 px-4 py-2">
                 2
               </div>
             </div>
@@ -261,10 +241,7 @@ const { format: formatNumber } = Intl.NumberFormat('en-GB', { notation: 'compact
             </div>
           </div>
         </div>
-        <UButton
-          to="/guide" size="xl" :label="section.button" variant="outline" color="transparent"
-          class="mt-8 w-fit"
-        />
+        <UButton to="/guide" size="xl" :label="section.button" variant="outline" color="transparent" class="mt-8 w-fit" />
       </div>
     </template>
   </ULandingSection>
