18427b0fa5a40c928fda7b58f6f7c94a78b85f6b
Sébastien Chopin <seb@nuxt.com>
2023-10-09 16:19:07 +0200

    docs: add fatal true

--- docs/pages/[...slug].vue
+++ docs/pages/[...slug].vue
@@ -6,7 +6,7 @@ const route = useRoute()
 
 const { data: page } = await useAsyncData(`docs-${route.path}`, () => queryContent(route.path).findOne())
 if (!page.value)
-  throw createError({ statusCode: 404, statusMessage: 'Page not found' })
+  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
 
 const { data: surround } = await useAsyncData(`docs-${route.path}-surround`, () => {
   return queryContent()
