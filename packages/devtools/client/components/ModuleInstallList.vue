0205298b824cc9a047a8cff17f4fe0052e94098d
Arash <38922203+arashsheyda@users.noreply.github.com>
2023-10-15 17:38:37 -0600

    feat(modules): dynamic sorting for module installation list

--- packages/devtools/client/components/ModuleInstallList.vue
+++ packages/devtools/client/components/ModuleInstallList.vue
@@ -2,13 +2,30 @@
 // @ts-expect-error missing types
 import { RecycleScroller } from 'vue-virtual-scroller'
 import Fuse from 'fuse.js'
+import type { ModuleStaticInfo } from '../../src/types'
+
+type SortingFunction<T> = (a: T, b: T) => number
 
 const emit = defineEmits(['close'])
 
 const collection = useModulesList()
 
+const sortingOptions = ['downloads', 'stars', 'updated', 'created'] as const
+const ascendingOrder = ref(false)
+const selectedSortingOption = ref<typeof sortingOptions[number]>(sortingOptions[0])
+
+const sortingFactors: Record<typeof sortingOptions[number], SortingFunction<ModuleStaticInfo>> = {
+  downloads: (a, b) => a.stats.downloads - b.stats.downloads,
+  stars: (a, b) => a.stats.stars - b.stats.stars,
+  created: (a, b) => a.stats.createdAt - b.stats.createdAt,
+  updated: (a, b) => a.stats.publishedAt - b.stats.publishedAt,
+}
+
+const sortedItems = computed(() => collection.value?.slice()
+  .sort((a, b) => sortingFactors[selectedSortingOption.value](a, b) * (ascendingOrder.value ? 1 : -1)))
+
 const search = ref('')
-const fuse = computed(() => new Fuse(collection.value || [], {
+const fuse = computed(() => new Fuse(sortedItems.value || [], {
   keys: [
     'name',
     'description',
@@ -19,7 +36,7 @@ const fuse = computed(() => new Fuse(collection.value || [], {
 
 const items = computed(() => {
   if (!search.value)
-    return collection.value
+    return sortedItems.value
   return fuse.value.search(search.value).map(r => r.item)
 })
 </script>
@@ -33,20 +50,51 @@ const items = computed(() => {
       text="Install Module"
     />
 
-    <NTextInput
-      v-model="search"
-      :autofocus="true"
-      placeholder="Search..."
-      icon="carbon-search" n="primary"
-      mx6 px-5 py-2
-    />
+    <NNavbar v-model:search="search" no-padding px-6 pb-5 pt-2>
+      <template #actions>
+        <NDropdown direction="end" n="sm primary">
+          <template #trigger="{ click }">
+            <div flex="~  items-center">
+              <NButton
+                :icon="ascendingOrder ? 'tabler:sort-ascending' : 'tabler:sort-descending'"
+                h-full rounded-r-none
+                @click="ascendingOrder = !ascendingOrder"
+              />
+              <NButton
+                flex="~ justify-between"
+                min-w-30 border-l-0 rounded-l-none px-2 capitalize
+                hover="border-l-1"
+                @click="click()"
+              >
+                {{ selectedSortingOption }}
+                <NIcon icon="carbon:chevron-down" />
+              </NButton>
+            </div>
+          </template>
+          <div flex="~ col" w-30 of-auto>
+            <NButton
+              v-for="item of sortingOptions"
+              :key="item"
+              :border="false" p2
+              hover="n-checkbox-hover text-green"
+              @click="selectedSortingOption = item"
+            >
+              <span flex="~ justify-between" w-full text-xs capitalize op75>
+                {{ item }}
+                <NIcon v-if="selectedSortingOption === item" icon="carbon:checkmark" />
+              </span>
+            </NButton>
+          </div>
+        </NDropdown>
+      </template>
+    </NNavbar>
 
     <div flex-auto of-auto flex="~ col gap-2" pl6 pr4>
       <RecycleScroller
         v-slot="{ item }"
         class="scroller"
         :items="items"
-        :item-size="160"
+        :item-size="200"
         key-field="name"
       >
         <ModuleItemInstall :item="item" @start="emit('close')" />
