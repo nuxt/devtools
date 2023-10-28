68efb1bab82b4bf5aa013df0221ab1268dcd221f
Daniel Roe <daniel@roe.dev>
2023-10-28 10:09:52 +0900

    fix(devtools): import app utilities from `#imports` (#500)

--- packages/devtools/src/runtime/plugins/devtools.server.ts
+++ packages/devtools/src/runtime/plugins/devtools.server.ts
@@ -1,6 +1,5 @@
 import type { TimelineServerState } from '../../types'
-import { defineNuxtPlugin } from '#app'
-import { useState } from '#imports'
+import { defineNuxtPlugin, useState } from '#imports'
 
 export default defineNuxtPlugin(() => {
   // record ssr start time
--- playgrounds/module-starter/src/runtime/plugin.ts
+++ playgrounds/module-starter/src/runtime/plugin.ts
@@ -1,4 +1,4 @@
-import { defineNuxtPlugin } from '#app'
+import { defineNuxtPlugin } from '#imports'
 
 export default defineNuxtPlugin((_nuxtApp) => {
   // eslint-disable-next-line no-console
