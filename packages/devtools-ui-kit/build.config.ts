f3c3de1d95ba5da08dd665c2918500b309adb2af
Emmanuel Salomon <emmanuel.salomon@gmail.com>
2024-01-15 12:53:42 +0100

    fix(ui): Added composable to build config (#579)

--- packages/devtools-ui-kit/build.config.ts
+++ packages/devtools-ui-kit/build.config.ts
@@ -6,6 +6,7 @@ export default defineBuildConfig({
     { input: 'src/module', format: 'esm' },
     { input: 'src/unocss', format: 'esm' },
     { input: 'src/components/', outDir: 'dist/components' },
+    { input: 'src/composables/', outDir: 'dist/composables' },
     { input: 'src/runtime/', outDir: 'dist/runtime' },
     { input: 'src/assets/', outDir: 'dist/assets' },
   ],
