5a9798333ff965d4c01e3a4f1de8d6a7b951414e
Daniel Roe <daniel@roe.dev>
2024-02-26 21:12:20 +0000

    refactor: use `createResolver` in playgrounds

--- packages/devtools-ui-kit/playground/nuxt.config.ts
+++ packages/devtools-ui-kit/playground/nuxt.config.ts
@@ -1,7 +1,9 @@
-import { resolve } from 'node:path'
 import { defineNuxtConfig } from 'nuxt/config'
+import { createResolver } from 'nuxt/kit'
 import '@unocss/nuxt'
 import DevtoolsUIKit from '../src/module'
+
+const resolver = createResolver(import.meta.url)
 
 export default defineNuxtConfig({
   modules: [
@@ -11,13 +13,13 @@ export default defineNuxtConfig({
     dev: true,
   },
   nitro: {
-    rootDir: resolve(__dirname, '..'),
+    rootDir: resolver.resolve('..'),
   },
   unocss: {
     preflight: true,
-    configFile: resolve(__dirname, '../unocss.config.ts'),
+    configFile: resolver.resolve('../unocss.config.ts'),
   },
   alias: {
-    '@nuxt/devtools-kit/iframe-client': resolve(__dirname, './stub/iframe-client.ts'),
+    '@nuxt/devtools-kit/iframe-client': resolver.resolve('./stub/iframe-client.ts'),
   },
 })
