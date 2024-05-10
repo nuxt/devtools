87199a174ed662dffc6d1eeaa04116635576fca6
Anthony Fu <anthonyfu117@hotmail.com>
2024-05-10 15:58:39 +0200

    fix: module builder chunk path patch

--- packages/devtools/build.config.ts
+++ packages/devtools/build.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath } from 'node:url'
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
@@ -21,5 +22,33 @@ export default defineBuildConfig({
   ],
   rollup: {
     inlineDependencies: true,
+  },
+  hooks: {
+    // Patch @nuxt/module-builder@0.6.0 not adding .mjs extension for runtime files
+    // https://github.com/nuxt/module-builder/issues/261
+    'rollup:options': (_, options) => {
+      options.plugins ||= []
+      if (!Array.isArray(options.plugins))
+        options.plugins = [options.plugins]
+
+      const runtimeDir = fileURLToPath(new URL('./src/runtime', import.meta.url))
+      options.plugins.unshift({
+        name: 'unbuild:runtime-build:patch',
+        async resolveId(id, importter) {
+          if (!id.includes('runtime'))
+            return
+          const resolved = await this.resolve(id, importter, { skipSelf: true })
+          if (resolved?.id.startsWith(runtimeDir)) {
+            let id = resolved.id
+            if (!id.endsWith('.mjs'))
+              id += '.mjs'
+            return {
+              external: true,
+              id,
+            }
+          }
+        },
+      })
+    },
   },
 })
