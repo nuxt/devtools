4188f8d3a2323873ef760aa2fe5b7b5faca22959
Anthony Fu <github@antfu.me>
2024-07-02 18:11:37 +0200

    fix: use `ofetch` for `fast-npm-meta`

--- packages/devtools/src/npm/index.ts
+++ packages/devtools/src/npm/index.ts
@@ -2,33 +2,12 @@ import { createRequire } from 'node:module'
 import { logger, useNuxt } from '@nuxt/kit'
 import { readPackageJSON } from 'pkg-types'
 import semver from 'semver'
+import { fetch } from 'ofetch'
 import { getPackageInfo } from 'local-pkg'
 import type { PackageUpdateInfo } from '../types'
 
 export async function getMainPackageJSON(nuxt = useNuxt()) {
   return readPackageJSON(nuxt.options.rootDir)
-}
-
-export interface Packument {
-  'name': string
-  /**
-   * An object where each key is a version, and each value is the manifest for
-   * that version.
-   */
-  'versions': Record<string, Omit<Packument, 'versions'>>
-  /**
-   * An object mapping dist-tags to version numbers. This is how `foo@latest`
-   * gets turned into `foo@1.2.3`.
-   */
-  'dist-tags': { latest: string } & Record<string, string>
-  /**
-   * In the full packument, an object mapping version numbers to publication
-   * times, for the `opts.before` functionality.
-   */
-  'time': Record<string, string> & {
-    created: string
-    modified: string
-  }
 }
 
 export async function checkForUpdateOf(name: string, current?: string, nuxt = useNuxt()): Promise<PackageUpdateInfo | undefined> {
@@ -45,7 +24,9 @@ export async function checkForUpdateOf(name: string, current?: string, nuxt = us
       return
 
     const { getLatestVersion } = await import('fast-npm-meta')
-    const { version: latest } = await getLatestVersion(name)
+    const { version: latest } = await getLatestVersion(name, {
+      fetch,
+    })
 
     const needsUpdate = !!latest && latest !== current && semver.lt(current, latest)
 
