8c983ea26dc15896c6e134b6438258f9b1322016
Anthony Fu <github@antfu.me>
2024-07-28 14:54:29 +0200

    chore: update deps

--- taze.config.ts
+++ taze.config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   exclude: [
     'execa', // Node compatibility,
     'which', // Node compatibility,
+
+    // Since v0.6.0, the runtime extension is not compatible with our usage to share the code for both the client and the server.
+    '@nuxt/module-builder',
   ],
   ignorePaths: [
     'clones',
