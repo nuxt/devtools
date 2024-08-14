ead81a8f878d1a2d726cbcb0fb416ff8226c656b
Anthony Fu <github@antfu.me>
2024-08-14 02:44:59 +0200

    chore: update deps

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -300,11 +300,7 @@ watch(projectsSectionVisible, () => {
   top: 25vh;
   width: 100%;
   height: 30vh;
-  background: radial-gradient(
-    50% 50% at 50% 50%,
-    #00dc82 0%,
-    rgba(0, 220, 130, 0) 100%
-  );
+  background: radial-gradient(50% 50% at 50% 50%, #00dc82 0%, rgba(0, 220, 130, 0) 100%);
   filter: blur(180px);
   opacity: 0.6;
   z-index: -1;
