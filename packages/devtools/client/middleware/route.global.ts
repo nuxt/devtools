300040a757bf4252ce490b2a914a2d95b4480493
Valentin Prugnaud <valentin@whatdafox.com>
2024-09-22 11:54:51 -0700

    fix: welcome page showing after every page reload

--- packages/devtools/client/middleware/route.global.ts
+++ packages/devtools/client/middleware/route.global.ts
@@ -4,6 +4,6 @@ import { telemetryEnabled } from '~/composables/telemetry'
 export default defineNuxtRouteMiddleware((to) => {
   if (to.path === '/' && !(isFirstVisit.value || telemetryEnabled.value == null))
     return navigateTo('/modules/overview')
-  else if (to.path !== '/' && telemetryEnabled.value == null)
+  else if (to.path !== '/' && isFirstVisit.value)
     return navigateTo('/')
 })
