d23ce0993d1baa0cdd119a86429d2f6271410887
Anthony Fu <anthonyfu117@hotmail.com>
2024-09-23 20:52:51 +0200

    fix: welcome page condition (#736)
    Co-authored-by: Valentin Prugnaud <valentin@whatdafox.com>

--- packages/devtools/client/middleware/route.global.ts
+++ packages/devtools/client/middleware/route.global.ts
@@ -1,9 +1,11 @@
 import { isFirstVisit } from '~/composables/storage'
-import { telemetryEnabled } from '~/composables/telemetry'
 
 export default defineNuxtRouteMiddleware((to) => {
-  if (to.path === '/' && !(isFirstVisit.value || telemetryEnabled.value == null))
+  if (isFirstVisit.value) {
+    if (to.path !== '/')
+      return navigateTo('/')
+  }
+  else if (to.path === '/') {
     return navigateTo('/modules/overview')
-  else if (to.path !== '/' && telemetryEnabled.value == null)
-    return navigateTo('/')
+  }
 })
