217e09be4ebaaeb0769c14c7e1d5f3fdde05c19c
Anthony Fu <github@antfu.me>
2024-10-12 13:14:37 +0800

    chore: update

--- packages/devtools/smoke.e2e.spec.ts
+++ packages/devtools/smoke.e2e.spec.ts
@@ -1,9 +1,16 @@
 import { expect, test } from '@playwright/test'
 
 test('DevTools renders', async ({ page }) => {
-  await page.goto('/')
+  await page.goto('./', { waitUntil: 'networkidle' })
+
+  await page.waitForTimeout(2000) // TODO: A smarter wait for loading state
 
   // Expect a heading to be visible (using this as a smoke test)
-  const heading = await page.getByRole('heading', { name: 'Nuxt DevTools' })
-  expect(heading).toBeVisible()
+  const welcome = await page.getByRole('heading', { name: 'Nuxt DevTools Welcome' })
+  expect(welcome).toBeVisible()
+
+  const getStarted = await page.getByRole('link', { name: 'Get Started' })
+  expect(getStarted).toBeVisible()
+
+  await getStarted.click()
 })
--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000/__nuxt_devtools__/client/',
+    baseURL: `http://localhost:3000/${CLIENT_BASE}/`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -74,7 +74,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm dev',
+    command: 'pnpm run fixture:empty',
 
     // Wait for a 2XX response for this URL
     // before considering the server running.
