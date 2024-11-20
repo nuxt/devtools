be243e42e0db1fa9694b9f9c7aa69760e0898cff
Arlo <webfansplz@gmail.com>
2024-11-20 17:35:20 +0800

    fix: missing pinia store modules (#751)

--- packages/devtools/client/setup/vue-devtools.ts
+++ packages/devtools/client/setup/vue-devtools.ts
@@ -1,18 +1,37 @@
-import { functions, onRpcConnected, rpc } from '@vue/devtools-core'
-import { createRpcClient, toggleHighPerfMode } from '@vue/devtools-kit'
+import { functions, onRpcConnected, rpc, useDevToolsState as useVueDevToolsState } from '@vue/devtools-core'
+import { createRpcClient } from '@vue/devtools-kit'
 import { watchEffect } from 'vue'
 import { useDevToolsFrameState } from '../composables/storage'
 
 export function setupVueDevTools() {
+  const { connected } = useVueDevToolsState()
+
   const state = useDevToolsFrameState()
 
   const isInPopup = window.__NUXT_DEVTOOLS_IS_POPUP__
 
+  function toggleClientDetected(state: boolean) {
+    if (connected.value) {
+      rpc.value.updateDevToolsClientDetected({
+        iframe: state,
+      })
+    }
+    else {
+      onRpcConnected(() => {
+        rpc.value.updateDevToolsClientDetected({
+          iframe: state,
+        })
+      })
+    }
+  }
+
   watchEffect(() => {
-    if (isInPopup)
-      toggleHighPerfMode(false)
-    else
-      toggleHighPerfMode(!state.value?.open)
+    if (isInPopup) {
+      toggleClientDetected(true)
+    }
+    else {
+      toggleClientDetected(state.value?.open ?? false)
+    }
   })
 
   createRpcClient(functions, {
