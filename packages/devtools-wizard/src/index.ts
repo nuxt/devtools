23215e08765752f705dab9b8e6afcf355173b6ed
Anthony Fu <github@antfu.me>
2025-01-18 17:27:27 +0800

    feat!: remove global installation wizard

--- packages/devtools-wizard/src/index.ts
+++ packages/devtools-wizard/src/index.ts
@@ -1,7 +1,6 @@
 import { consola } from 'consola'
 import { colors } from 'consola/utils'
 import { readPackageJSON } from 'pkg-types'
-import semver from 'semver'
 import { name as moduleName, version } from '../package.json'
 
 async function getNuxtVersion(path: string): Promise<string | null> {
@@ -35,23 +34,13 @@ async function run() {
     consola.error('Unable to find any installed nuxt version in the current directory')
     process.exit(1)
   }
-  // Nuxt 3.4.0 will have devtools built-in
-  // https://github.com/nuxt/nuxt/pull/20126
-  const isDevToolsBuiltIn = semver.gte(nuxtVersion, '3.4.0')
-
   if (command === 'enable') {
     consola.log(colors.green('Enabling Nuxt DevTools...'))
-    if (isDevToolsBuiltIn)
-      await import('./builtin').then(r => r.enable(cwd))
-    else
-      await import('./global').then(r => r.enable(cwd))
+    await import('./builtin').then(r => r.enable(cwd))
   }
   else if (command === 'disable') {
     consola.log(colors.magenta('Disabling Nuxt DevTools...'))
-    if (isDevToolsBuiltIn)
-      await import('./builtin').then(r => r.disable(cwd))
-    else
-      await import('./global').then(r => r.disable(cwd, args))
+    await import('./builtin').then(r => r.disable(cwd))
   }
   else if (!command) {
     consola.log(`npx ${moduleName} enable|disable`)
