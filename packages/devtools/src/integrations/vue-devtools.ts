a1743f3fb6f6bb4cf8ba5b0fa2750794181bd2a1
Anthony Fu <github@antfu.me>
2025-02-06 20:05:49 +0800

    chore: update

--- packages/devtools/src/integrations/vue-devtools.ts
+++ packages/devtools/src/integrations/vue-devtools.ts
@@ -1,15 +1,15 @@
 import type { NuxtDevtoolsServerContext } from '../types'
-import { addPluginTemplate } from '@nuxt/kit'
+import { addPluginTemplate, resolvePath } from '@nuxt/kit'
 import { join } from 'pathe'
 import { runtimeDir } from '../dirs'
 
-export function setup({ nuxt }: NuxtDevtoolsServerContext) {
+export async function setup({ nuxt }: NuxtDevtoolsServerContext) {
   if (!nuxt.options.dev || nuxt.options.test)
     return
 
   addPluginTemplate({
     name: 'vue-devtools-client',
     mode: 'client',
-    src: join(runtimeDir, 'vue-devtools-client.js'),
+    src: await resolvePath(join(runtimeDir, 'vue-devtools-client')),
   })
 }
