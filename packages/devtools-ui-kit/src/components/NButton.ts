d6e2583c198542942f5c0cf4bac216076be506be
Anthony Fu <github@antfu.me>
2025-02-16 15:38:10 +0800

    chore: fix type error

---
+++ packages/devtools-ui-kit/src/components/NButton.ts
@@ -0,0 +1,46 @@
+import type { PropType } from 'vue'
+import { NuxtLink } from '#components'
+import { defineComponent, h, renderSlot } from 'vue'
+import NIcon from './NIcon.vue'
+
+// @unocss-include
+export default defineComponent({
+  name: 'NButton',
+  props: {
+    to: String,
+    icon: String,
+    border: {
+      type: Boolean,
+      default: true,
+    },
+    disabled: Boolean,
+    type: {
+      type: String as PropType<'submit' | 'reset' | 'button'>,
+      default: 'button',
+    },
+  },
+  setup(props, { attrs, slots }) {
+    return () => h(props.to ? NuxtLink : 'button', {
+      to: props.to,
+      ...attrs,
+      ...(!props.to && { type: props.type }),
+      ...(props.disabled ? { disabled: true } : { tabindex: 0 }),
+      // @ts-expect-error ignore type
+      class: [
+        props.border ? 'n-button-base active:n-button-active focus-visible:n-focus-base hover:n-button-hover' : '',
+        slots.default ? '' : 'n-icon-button',
+        'n-button n-transition n-disabled:n-disabled',
+      ].join(' '),
+    }, [
+      renderSlot(slots, 'icon', {}, () => props.icon
+        ? [
+            h(NIcon, {
+              icon: props.icon,
+              class: slots.default ? 'n-button-icon' : '',
+            }),
+          ]
+        : []),
+      renderSlot(slots, 'default'),
+    ])
+  },
+})
