cbbb502818e148555ec39ef6441ba97c2b1e1fd8
Julien Huang <julien.h.dev@gmail.com>
2025-04-13 22:01:51 +0200

    fix(ui-kit): add `@unocss-include` magic string in NButton

--- packages/devtools-ui-kit/src/components/NButton.ts
+++ packages/devtools-ui-kit/src/components/NButton.ts
@@ -3,7 +3,8 @@ import { NuxtLink } from '#components'
 import { defineComponent, h, renderSlot } from 'vue'
 import NIcon from './NIcon.vue'
 
-// @unocss-include
+// eslint-disable-next-line ts/no-unused-expressions
+'@unocss-include' // this is a hack to include unocss styles in the build
 export default defineComponent({
   name: 'NButton',
   props: {
