91dfe0266205a4476375ecab561dcbf3541d7a61
Anthony Fu <github@antfu.me>
2025-06-04 10:47:48 +0900

    chore: fix type

--- packages/devtools-ui-kit/src/runtime/client.ts
+++ packages/devtools-ui-kit/src/runtime/client.ts
@@ -1,3 +1,5 @@
+import type { NuxtDevtoolsIframeClient } from '@nuxt/devtools-kit/types'
+import type { Ref } from 'vue'
 import { useDevtoolsClient } from '@nuxt/devtools-kit/iframe-client'
 
-export const devToolsClient = useDevtoolsClient()
+export const devToolsClient: Ref<NuxtDevtoolsIframeClient | undefined> = useDevtoolsClient()
