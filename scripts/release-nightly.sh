dba26a6f36a05e43bb38b002d7abd5eac6d49250
Anthony Fu (via agent) <reg-github-bot@antfu.me>
2026-09-15 07:56:14 +0000

    fix(ci): build with bumped nightly names before publishing

--- scripts/release-nightly.sh
+++ scripts/release-nightly.sh
@@ -8,6 +8,9 @@ git restore -s@ -SW  -- packages
 # Bump versions to nightly
 pnpm jiti ./scripts/bump-nightly
 
+# Build with the bumped names/versions
+pnpm build
+
 # Update token
 if [[ ! -z ${NODE_AUTH_TOKEN} ]] ; then
   echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
